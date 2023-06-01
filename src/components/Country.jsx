107c7d45afcab7d605382bf5534a2e44315c076f
IgnasStrel <ignaslab46@gmail.com>
2023-06-01 23:44:31 +0300

    pridejau regionus ir t .t.

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,25 +1,38 @@
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const Country = ({allCountries}) => {
-    console.log(allCountries)
+const Country = ({ allCountries }) => {
+  console.log(allCountries);
   return (
-      <div>
-          {allCountries !== undefined && allCountries.length !== 0 ?
-          allCountries.map((country, index) => (
-    <Card style={{ width: '18rem' }} key={index}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>{country.name.common}</Card.Title>
-        <Card.Text>
-        kazkas
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-     ))
-      :<h1>Duomenys nerasti</h1>
-    }
-    </div>)
-        }
-export default Country
+    <div>
+      {allCountries !== undefined && allCountries.length !== 0 ? (
+        allCountries.map((country, index) => (
+          <Card style={{ width: "18rem" }} key={index}>
+            <Card.Img variant="top" src={country.flags.png} />
+            <Card.Body>
+              <Card.Title>{country.name.common}</Card.Title>
+              <Card.Text>
+                <p>
+                  <b>Population: </b>
+                  {country.population}
+                </p>
+              </Card.Text>
+              <Card.Text>
+                <b>Region: </b>
+                {country.region}
+              </Card.Text>
+              <Card.Text>
+                <b>Capital: </b>
+                {country.capital}
+              </Card.Text>
+              <Button variant="primary">More</Button>
+            </Card.Body>
+          </Card>
+        ))
+      ) : (
+        <h1>Duomenys nerasti</h1>
+      )}
+    </div>
+  );
+};
+export default Country;
